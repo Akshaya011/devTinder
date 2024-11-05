977c36ed5f0858bd50f568dedb2f9d11a64d8606
Akshaya011 <akshayaraj51182@gmail.com>
2024-11-06 01:46:12 +0530

    exporing get,post,delete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # devTinder
+-initialize git 
+.gitignore
+-create a remote repo on github
+-push all code to remote origin
+-insatlled postman api
+-logic for handle GET,POST,PUT,PACH,DELETE API calls and them on postman
--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 
 const app = express();
+
+app.get("/user", (req,res)=>{
+    res.send({firstname:"Akshaya ", lastname : "kumar"})
+})
+app.post("/user",(req,res)=>{
+    res.send("data successfully saved to the database!")
+})
+app.post("/user",(req,res)=>{
+    res.send("data successfully saved to the database!")
+})
+
 app.use("/test",(req,res)=> {
     res.send("hello from the server");
 })
